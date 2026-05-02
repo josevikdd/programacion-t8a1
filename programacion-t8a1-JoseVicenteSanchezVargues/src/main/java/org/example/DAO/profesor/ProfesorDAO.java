dc34e5aec3f534fa29e74028cab39c4a41926cb0
José Vicente Sánchez Vargues <josevi97sgb@gmail.com>
2026-05-02 20:23:21 +0200

    Crear DAO para la clase Profesor.

---
+++ programacion-t8a1-JoseVicenteSanchezVargues/src/main/java/org/example/DAO/profesor/ProfesorDAO.java
@@ -0,0 +1,7 @@
+package org.example.DAO.profesor;
+
+import org.example.DAO.IOperationsCRUD;
+import org.example.modelo.Profesor;
+
+public interface ProfesorDAO extends IOperationsCRUD<Profesor> {
+}
---
+++ programacion-t8a1-JoseVicenteSanchezVargues/src/main/java/org/example/DAO/profesor/ProfesorDAOImpl.java
@@ -0,0 +1,25 @@
+package org.example.DAO.profesor;
+
+import org.example.modelo.Profesor;
+
+public class ProfesorDAOImpl implements ProfesorDAO {
+    @Override
+    public Profesor findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public int add(Profesor object) {
+        return 0;
+    }
+
+    @Override
+    public int update(Profesor object) {
+        return 0;
+    }
+
+    @Override
+    public int deleteById(Long id) {
+        return 0;
+    }
+}
